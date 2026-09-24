f793025af2562661f6bc4e4acaaea0326d889a49
Andrew <belinskiy.and@gmail.com>
2026-09-24 23:16:30 +0300

    add header

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,2 +1,22 @@
-// remove this code
-console.log("mobile-menu");
+const openMobileMenuBtn = document.querySelector('.js-open-mobile-menu');
+const closeMobileMenuBtn = document.querySelector('.js-close-mobile-menu');
+const mobileMenuElem = document.querySelector('.js-mobile-menu');
+const mobileMenuListElem = document.querySelector('.js-mobile-menu-list');
+
+openMobileMenuBtn.addEventListener('click', onOpenMobileMenu);
+closeMobileMenuBtn.addEventListener('click', onCloseMobileMenu);
+mobileMenuListElem.addEventListener('click', onCheckCloseMobileMenu);
+
+function onOpenMobileMenu() {
+  mobileMenuElem.classList.add('is-open');
+}
+
+function onCloseMobileMenu() {
+  mobileMenuElem.classList.remove('is-open');
+}
+
+function onCheckCloseMobileMenu(e) {
+  if (e.target !== e.currentTarget) {
+    onCloseMobileMenu();
+  }
+}
